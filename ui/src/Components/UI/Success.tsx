f4af4edd3fa7eb0ba9aace092d1652571ea3a18b
This-Marinob-guy <vladislavmarinov3142@gmail.com>
2024-01-08 17:55:34 +0200

    end of day

--- ui/src/Components/UI/Success.tsx
+++ ui/src/Components/UI/Success.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { observer } from "mobx-react-lite"
 import {useNotification} from "../../store/NotificationStore";
 
-const Notification = observer(() => {
+const Success = observer(() => {
 
-    const {notification} = useNotification();
+    const {success} = useNotification();
 
     return (
         <div
             className="mb-4 border rounded-md shadow-sm border-green-200 text-green-500 bg-green-50 p-2 m-2"
         >
-            <h3 className='mr-3 font-bold'>{notification.code}</h3>
-            <p>{notification.message}</p>
+            <h3 className='mr-3 font-bold'>{success.code}</h3>
+            <p>{success.message}</p>
         </div>
     );
 });
 
-export default Notification;
+export default Success;
